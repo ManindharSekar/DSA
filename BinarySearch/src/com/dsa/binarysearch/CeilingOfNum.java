b69e87e5f703ef63a4fee9d97de801710ad17264
ManindharSekar <manindharsekar@gmail.com>
2026-07-31 14:57:23 +0530

    ceiling and Floor of Num BS

---
+++ BinarySearch/src/com/dsa/binarysearch/CeilingOfNum.java
@@ -0,0 +1,35 @@
+package com.dsa.binarysearch;
+
+public class CeilingOfNum {
+	
+	// ceiling = The smallest element in the array that is greater than or equal to the target.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {3,5,6,9,12,15,33,49,88};
+		int target=100;
+		int searchNum = searchNum(arr,target);
+		System.out.println(searchNum);
+
+	}
+	
+	static int searchNum(int[] arr, int target) {
+		if(target>=arr[arr.length-1]) {
+			return -1;
+		}
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			if(target<arr[mid]) {
+				end=mid-1;
+			}else if(target>arr[mid]) {
+				start=mid+1;
+			}else {
+				return mid;
+			}
+		}
+		return start;
+	}
+
+}
---
+++ BinarySearch/src/com/dsa/binarysearch/FloorOfNum.java
@@ -0,0 +1,32 @@
+package com.dsa.binarysearch;
+
+public class FloorOfNum {
+	
+	// Floor = The largest element in the array that is less than or equal to the target.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {3,6,8,10,15,18,22,27,50,70};
+		int target=25;
+		int searchNum = searchNum(arr,target);
+		System.out.println(searchNum);
+
+	}
+	
+	static int searchNum(int[] arr, int target) {
+		int start=0;
+		int end=arr.length-1;
+		while(start<=end) {
+			int mid=start+(end-start)/2;
+			if(target> arr[mid]) {
+				start=mid+1;
+			}else if(target<arr[mid]) {
+				end=mid-1;
+			}else {
+				return mid;
+			}
+		}
+		return end;
+	}
+
+}
