264f36f7f5b65985d5dde2f911eff9757c638e11
ManindharSekar <manindharsekar@gmail.com>
2026-08-10 15:08:54 +0530

    Missing Number Cyclic sort

---
+++ CycleSort/src/com/cyclesort/MissingNumber.java
@@ -0,0 +1,36 @@
+package com.cyclesort;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 4, 1, 0 };
+		System.out.println(sort(arr));
+
+	}
+
+	static int sort(int arr[]) {
+		int i = 0;
+		while (i < arr.length) {
+			int correct = arr[i];
+			if (arr[i] < arr.length && arr[i] != arr[correct]) {
+				swap(arr, i, correct);
+			} else {
+				i++;
+			}
+		}
+		for (int index = 0; index < arr.length; index++) {
+			if (arr[index] != index) {
+				return index;
+			}
+		}
+		return arr.length;
+	}
+
+	private static void swap(int[] arr, int first, int second) {
+		int temp = arr[first];
+		arr[first] = arr[second];
+		arr[second] = temp;
+	}
+
+}
